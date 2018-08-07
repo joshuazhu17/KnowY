a30c9ebd3a443d5c22152655fbd0becc6a9d6e15
joshuazhu17 <joshua.zhu@gmail.com>
2018-08-07 14:13:17 -0700

    Changed tutorial colors

--- KnowY/KnowY/Views/FirstTutorialLastPageViewController.swift
+++ KnowY/KnowY/Views/FirstTutorialLastPageViewController.swift
@@ -15,6 +15,7 @@ class FirstTutorialLastPageViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        doneButton.layer.cornerRadius = 12
     }
 
     override func didReceiveMemoryWarning() {
